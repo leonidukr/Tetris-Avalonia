309fb7838b42d68a0a8fe863cc830bf2559b3a4c
yura <yurasherbatov2003@gmail.com>
2025-12-20 03:27:10 +0700

    Исправил баг с сохранением имени, теперь имя в таблице результатов отображается корректно

--- TetrisAvalonia/MainWindow.axaml.cs
+++ TetrisAvalonia/MainWindow.axaml.cs
@@ -309,10 +309,13 @@ namespace TetrisAvalonia
         {
             // ��������� ��� ������
             _playerName = string.IsNullOrWhiteSpace(PlayerNameTextBox.Text) ?
-                "Player" : PlayerNameTextBox.Text.Trim();
+            "Player" : PlayerNameTextBox.Text.Trim();
 
             if (_playerName.Length > 10)
                 _playerName = _playerName.Substring(0, 10);
+
+            // Передаем имя в игру
+            _game.PlayerName = _playerName;
 
             // ������������� �� ����� ����
             MenuScreen.IsVisible = false;
